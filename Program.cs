8fd1e9d624c1690db12a7acde9d6236cfde939ac
azick7553 <azick7553@gmail.com>
2020-04-04 18:05:23 +0500

    Added models(Account,Customer,Currency,PaymentHistory)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,79 @@ namespace payment_system
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account acc1 = new Account() { Id = 1, AccountNumber = new Random().Next(1, 1000).ToString() };
+            Account acc2 = new Account() { Id = 2, AccountNumber = new Random().Next(1, 1000).ToString() };
+            Account acc3 = new Account() { Id = 3, AccountNumber = new Random().Next(1, 1000).ToString() };
+            Currency currTjs = new Currency()
+            {
+                Id = 1,
+                ShortName = "TJS"
+            };
+            Currency currUSD = new Currency()
+            {
+                Id = 2,
+                ShortName = "USD"
+            };
+
+            acc1.AccountCurrency = currTjs;
+            acc2.AccountCurrency = currTjs;
+            acc3.AccountCurrency = currUSD;
+
+            Customer cust1 = new Customer()
+            {
+                Id = 1,
+                LastName = "Test",
+                FirstName = "Test",
+                MiddleName = "Test",
+                Accounts = new Account[] { acc1, acc2, acc3 }
+            };
+
+            if (cust1.Accounts != null)
+            {
+                foreach (var custAccounts in cust1.Accounts)
+                {
+                    System.Console.WriteLine($"Номер счета: {custAccounts.AccountNumber}\nВалюта счета:{custAccounts.AccountCurrency.ShortName}");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("У клиента нету счетов!!!");
+            }
+
+
+
+
         }
     }
+    class Account
+    {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public Currency AccountCurrency { get; set; }
+        public decimal AccountBalance { get; set; }
+        public PaymentHistory AccountPaymentHistory { get; set; }
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public Account[] Accounts { get; set; }
+    }
+
+    class Currency
+    {
+        public int Id { get; set; }
+        public string ShortName { get; set; }
+    }
+    class PaymentHistory
+    {
+        public int Id { get; set; }
+        public Currency PaymentCurrenct { get; set; }
+        public decimal PaymantAmount { get; set; }
+    }
+
+
 }
